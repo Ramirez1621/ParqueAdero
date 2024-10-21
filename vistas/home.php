05a6f6cb00cb5ab3a6af7f4a2c9f296aafaf88b3
Ramirez1621 <ferneyramirez2105@gmail.com>
2024-10-20 20:50:11 -0500

    1

---
+++ vistas/home.php
@@ -0,0 +1,14 @@
+<div class="container is-fluid">
+	<h1 class="title">Home</h1>
+	<h2 class="subtitle">¡Bienvenido <?php echo $_SESSION['nombre']." ".$_SESSION['apellido']; ?>!</h2>
+	<!-- <div class="box">
+        <span style="--i:1;"><img src="./homeimg/img/image1.jpg" alt=""></span>
+        <span style="--i:2;"><img src="./homeimg/img/image2.jpg" alt=""></span>
+        <span style="--i:3;"><img src="./homeimg/img/image3.jpg" alt=""></span>
+        <span style="--i:4;"><img src="./homeimg/img/image4.jpg" alt=""></span>
+        <span style="--i:5;"><img src="./homeimg/img/image5.jpg" alt=""></span>
+        <span style="--i:6;"><img src="./homeimg/img/image6.jpg" alt=""></span>
+        <span style="--i:7;"><img src="./homeimg/img/image7.jpg" alt=""></span>
+        <span style="--i:8;"><img src="./homeimg/img/image8.jpg" alt=""></span>
+    </div> -->
+</div>
